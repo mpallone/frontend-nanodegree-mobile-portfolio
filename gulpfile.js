85f3b0e40090b19fb33cb5b4b6a3848526241c61
Mark Pallone <mark.c.pallone@gmail.com>
2017-04-22 14:57:34 -0400

    Set up JS and CSS minifying

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,32 @@
-var gulp = require('gulp');
+var gulp     = require('gulp');
+var minify   = require('gulp-minify');
+var cleanCSS = require('gulp-clean-css');
 
 gulp.task('default', function() {
   // place code for your default task here
+});
+
+gulp.task('printDone', function() {
+    console.log("Done!!!");
+});
+
+gulp.task('compressJs', function() {
+    gulp.src('src/**/*.js') // todo - test that this works on all JS files, in different locations
+        .pipe(minify({
+            ext:{
+                src:'-debug.js',
+                min:'.min.js'
+            }
+        }))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('minify-css', function() {
+  return gulp.src('src/**/*.css') // todo - test that this works on all JS files, in different locations
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', function(){
+    gulp.watch('src/**', ['compressJs', 'minify-css', 'printDone']);
 });
